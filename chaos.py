51835b42d1f0fe1676000ee533c73d6cbd905f4d
Marco Sandoval <marcosandoval68@gmail.com>
2020-06-20 20:21:27 -0700

    SOLID

--- chaos.py
+++ chaos.py
@@ -1,4 +1,4 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 from collections import namedtuple
 import random
@@ -19,26 +19,31 @@ def build_visual(anchors=[], moving_point=None,):
 
     print('anchors:', anchors)
     print('moving_point:', moving_point)
-    pass
+
+
+def random_point():
+    # return Point(x=random.randrange(0, 20), y=random.randrange(0, 20))
+
+    return [random.randrange(0, 20), random.randrange(0, 20)]
 
 
 def generate_anchor_points(n=2):
     anchor_points = []
 
     for i in range(n):
-        anchor = Point(x=random.randrange(-10, 10), y=random.randrange(-10, 10))
-        anchor_points.append(anchor)
+        anchor_points.append(random_point())
 
     return anchor_points
 
 
 def generate_starting_point():
-    starting_point = Point(x=random.randrange(-10, 10), y=random.randrange(-10, 10))
+    starting_point = random_point()
     return starting_point
 
 
 if __name__ == "__main__":
+    random.seed(100)
     num_anchor_points = 3
     anchor_points = generate_anchor_points(n=num_anchor_points)
-    starting_position = Point() # at random
+    starting_position = generate_starting_point()
     build_visual(anchors=anchor_points, moving_point=starting_position)
