dd4f8e12fe72b8c552c59427e989184cd3bcd164
Marco Sandoval <marcosandoval68@gmail.com>
2020-06-20 20:00:21 -0700

    randomizing points

--- chaos.py
+++ chaos.py
@@ -1,6 +1,7 @@
 import matplotlib as plt
 import numpy as np
 from collections import namedtuple
+import random
 
 Point = namedtuple('Point', 'x y')
 
@@ -15,11 +16,18 @@ def mid_point(A, B, portion=2):
 def build_visual(anchors=[], moving_point=None,):
     #TODO: Build anchor points
     #TODO: build plt
+
+    print('anchors:', anchors)
+    print('moving_point:', moving_point)
     pass
 
 
 def generate_anchor_points(n=2):
     anchor_points = []
+
+    for i in range(n):
+        anchor = Point(x=random.randrange(-10, 10), y=random.randrange(-10, 10))
+        anchor_points.append(anchor)
 
     return anchor_points
 
